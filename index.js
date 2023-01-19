8eae3a918d9ea4c5ea5bff0103a8a5432896c65d
Ghutoon <niladrichatterjeen@gmail.com>
2023-01-19 13:39:49 +0530

    fixes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 
 const app = express()
-const PORT = 4000
+const PORT = 8080
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
@@ -12,9 +12,6 @@ app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
 
 app.get('/verification', (req, res) =>{
     if (req.query["hub.verify_token"] == "niladri") {
