b00c5fac3a4dac083326b03198b8acfc32d09f47
Ghutoon <niladrichatterjeen@gmail.com>
2023-01-19 16:34:37 +0530

    ajsfhadjfs

--- index.js
+++ index.js
@@ -41,71 +41,41 @@ app.get('/verification', (req, res) => {
 });
 
 var to, text;
-lock = false;
-counter = 1;
 app.post('/verification', (req, res) => {
-
-  console.log(`hello world ${counter}`);
-  counter += 1;
+  console.dir(req.body, {
+    depth: null
+  })
+  to = req.body.entry[0].changes[0].value["messages"][0]["from"];
+  text = req.body.entry[0].changes[0].value["messages"][0]["text"]["body"];
   res.sendStatus(200);
-  // if (lock == false) {
-  //   lock = true;
-
-
-  //   console.dir(req.body, {
-  //     depth: null
-  //   });
-  //   try {
-  //     to = req.body.entry[0].changes[0].value["messages"][0]["from"];
-  //     text = req.body.entry[0].changes[0].value["messages"][0]["text"]["body"];
-  //     console.log(to);
-  //     console.log(text);
-  //     res.sendStatus(200);
-
-  //     var data = JSON.stringify({
-  //       "messaging_product": "whatsapp",
-  //       "to": '917044174529',
-  //       "type": "text",
-  //       "text": {
-  //         "body": from
-  //       }
-  //     });
-
-  //     var config = {
-  //       method: 'post',
-  //       url: 'https://graph.facebook.com/v15.0/101412459527848/messages',
-  //       headers: {
-  //         'Authorization': `Bearer ${process.env.TOKEN}`,
-  //         'Content-Type': 'application/json'
-  //       },
-  //       data: data
-  //     };
-
-  //     axios(config)
-  //       .then(function (response) {
-  //         console.log(JSON.stringify(response.data));
-  //         lock = true;
-  //       })
-  //       .catch(function (error) {
-  //         console.log("some error happened with axios");
-  //         lock = true;
-  //       });
-
-
-
-  //   } catch (error) {
-  //     lock = true;
-  //     console.log("error");
-  //     res.sendStatus(401);
-  //   }
-
-  // }
-
-
-
 });
 
+var data = JSON.stringify({
+  "messaging_product": "whatsapp",
+  "to": to,
+  "type": text,
+  "text": {
+    "body": "hey"
+  }
+});
 
+var config = {
+  method: 'post',
+  url: 'https://graph.facebook.com/v15.0/101412459527848/messages',
+  headers: {
+    'Authorization': `Bearer ${process.env.TOKEN}`,
+    'Content-Type': 'application/json'
+  },
+  data: data
+};
+
+axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
