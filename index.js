ec62589d3a2323219ad38ec7f475a2a79888159c
Ghutoon <niladrichatterjeen@gmail.com>
2023-01-19 16:11:40 +0530

    consol log in post

--- index.js
+++ index.js
@@ -43,60 +43,61 @@ app.get('/verification', (req, res) => {
 var to, text;
 lock = false;
 
-app.post('/verification', async (req, res) => {
+app.post('/verification', (req, res) => {
 
-  if (lock == false) {
-    lock = true;
-
-
-    console.dir(req.body, {
-      depth: null
-    });
-    try {
-      to = req.body.entry[0].changes[0].value["messages"][0]["from"];
-      text = req.body.entry[0].changes[0].value["messages"][0]["text"]["body"];
-      console.log(to);
-      console.log(text);
-      res.sendStatus(200);
-
-      var data = JSON.stringify({
-        "messaging_product": "whatsapp",
-        "to": '917044174529',
-        "type": "text",
-        "text": {
-          "body": from
-        }
-      });
-
-      var config = {
-        method: 'post',
-        url: 'https://graph.facebook.com/v15.0/101412459527848/messages',
-        headers: {
-          'Authorization': `Bearer ${process.env.TOKEN}`,
-          'Content-Type': 'application/json'
-        },
-        data: data
-      };
-
-      axios(config)
-        .then(function (response) {
-          console.log(JSON.stringify(response.data));
-          lock = true;
-        })
-        .catch(function (error) {
-          console.log("some error happened with axios");
-          lock = true;
-        });
+  console.log("hello world");
+  // if (lock == false) {
+  //   lock = true;
 
 
+  //   console.dir(req.body, {
+  //     depth: null
+  //   });
+  //   try {
+  //     to = req.body.entry[0].changes[0].value["messages"][0]["from"];
+  //     text = req.body.entry[0].changes[0].value["messages"][0]["text"]["body"];
+  //     console.log(to);
+  //     console.log(text);
+  //     res.sendStatus(200);
 
-    } catch (error) {
-      lock = true;
-      console.log("error");
-      res.sendStatus(401);
-    }
+  //     var data = JSON.stringify({
+  //       "messaging_product": "whatsapp",
+  //       "to": '917044174529',
+  //       "type": "text",
+  //       "text": {
+  //         "body": from
+  //       }
+  //     });
 
-  }
+  //     var config = {
+  //       method: 'post',
+  //       url: 'https://graph.facebook.com/v15.0/101412459527848/messages',
+  //       headers: {
+  //         'Authorization': `Bearer ${process.env.TOKEN}`,
+  //         'Content-Type': 'application/json'
+  //       },
+  //       data: data
+  //     };
+
+  //     axios(config)
+  //       .then(function (response) {
+  //         console.log(JSON.stringify(response.data));
+  //         lock = true;
+  //       })
+  //       .catch(function (error) {
+  //         console.log("some error happened with axios");
+  //         lock = true;
+  //       });
+
+
+
+  //   } catch (error) {
+  //     lock = true;
+  //     console.log("error");
+  //     res.sendStatus(401);
+  //   }
+
+  // }
 
 
 
