ffc8e891d0452ccf2eda182e66f97194f0d643fc
Ghutoon <niladrichatterjeen@gmail.com>
2023-01-19 13:29:24 +0530

    Verification added

--- index.js
+++ index.js
@@ -16,5 +16,19 @@ app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
 })
 
+app.get('/verification', (req, res) =>{
+    if (req.query["hub.verify_token"] == "niladri") {
+
+    console.log("Verifying token");
+    res.send(req.query["hub.challenge"]);
+
+    console.log("Verifying token2");
+  }
+  else{
+    console.log("Token not verified");
+    res.sendStatus(403);
+  }
+});
+
 // Export the Express API
 module.exports = app
